d5f467a50f6d0be80601e65bcff3233e9d0a5e85
psytau <rmanninoFoo@gmail.com>
2019-05-31 17:42:24 +0800

    add gunicorn startup app

---
+++ application.py
@@ -0,0 +1,9 @@
+import server
+
+application = server.app
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
